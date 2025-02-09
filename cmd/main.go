0dad285a381d5c5eb2822214777a28c4efac3ab3
anijackich <anijackich@gmail.com>
2025-02-09 21:32:27 +0300

    feat(app-logic): add handlers

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"go-url-shortener/internal/config"
+	"go-url-shortener/internal/handlers"
+	"go-url-shortener/internal/repository/memory"
+	"go-url-shortener/internal/service"
 )
 
 func main() {
-	cfg := config.LoadConfig()
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Cannot load config: %s", err.Error())
+		return
+	}
+
 	addr := fmt.Sprintf("%s:%d", cfg.App.Host, cfg.App.Port)
+
+	linkRepo := memory.NewLinkRepository()
+
+	linkService, err := service.NewLinkService(
+		cfg.App.Domain,
+		cfg.App.LinkCodeAlphabet,
+		cfg.App.LinkCodeLength,
+		linkRepo,
+	)
+	if err != nil {
+		log.Fatalf("Cannot init link service: %s", err.Error())
+		return
+	}
+
+	linkHandler := handlers.NewLinkHandler(*linkService)
 
 	r := gin.Default()
 
-	r.Run(addr)
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("/shorten", linkHandler.ShortenLink)
+		v1.GET("/expand", linkHandler.ExpandLink)
+	}
+
+	err = r.Run(addr)
+	if err != nil {
+		log.Fatalf("Failed to launch server: %s", err.Error())
+		return
+	}
 }
--- internal/handlers/links.go
+++ internal/handlers/links.go
@@ -1,1 +1,81 @@
 package handlers
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"go-url-shortener/internal/repository"
+	"go-url-shortener/internal/service"
+	"net/http"
+)
+
+type LongLink struct {
+	URL string `json:"long_url"`
+}
+
+type ShortenedLink struct {
+	URL string `json:"short_url"`
+}
+
+type LinkHandler struct {
+	service service.LinkService
+}
+
+func NewLinkHandler(service service.LinkService) *LinkHandler {
+	return &LinkHandler{service: service}
+}
+
+func (h *LinkHandler) ShortenLink(c *gin.Context) {
+	var longLink LongLink
+
+	err := c.BindJSON(&longLink)
+	if err != nil {
+		c.JSON(
+			http.StatusUnprocessableEntity,
+			gin.H{"error": err},
+		)
+	}
+
+	shortenedLink, err := h.service.ShortenLink(longLink.URL)
+	if err != nil {
+		switch {
+		case errors.Is(err, repository.ErrLinkAlreadyExists):
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		case errors.Is(err, service.ErrInvalidURL):
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		default:
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		ShortenedLink{
+			URL: shortenedLink,
+		},
+	)
+}
+
+func (h *LinkHandler) ExpandLink(c *gin.Context) {
+	shortLink := c.Query("u")
+
+	expandedLink, err := h.service.ExpandShortLink(shortLink)
+	if err != nil {
+		switch {
+		case errors.Is(err, repository.ErrLinkNotFound):
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		case errors.Is(err, service.ErrInvalidURL):
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		default:
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		LongLink{
+			URL: expandedLink,
+		},
+	)
+}
