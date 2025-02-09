b1c3230c05747f187db2c55e307193fea77c42b4
anijackich <anijackich@gmail.com>
2025-02-09 14:50:59 +0300

    feat(repository): add link repository interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,8 @@
 package repository
+
+import "go-url-shortener/internal/models"
+
+type LinkRepository interface {
+	CreateLink(link *models.Link) error
+	GetLinkByCode(code string) (*models.Link, error)
+}
