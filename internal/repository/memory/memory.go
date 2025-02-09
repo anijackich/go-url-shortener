d76a83ea46546979ef60a25b98b8a982a9cc5444
anijackich <anijackich@gmail.com>
2025-02-09 15:05:36 +0300

    feat(repository): add memory storage

--- internal/repository/memory/memory.go
+++ internal/repository/memory/memory.go
@@ -1,1 +1,45 @@
 package memory
+
+import (
+	"go-url-shortener/internal/models"
+	"go-url-shortener/internal/repository"
+	"sync"
+)
+
+type LinkRepository struct {
+	links map[string]*models.Link
+	mu    sync.RWMutex
+}
+
+func NewLinkRepository() *LinkRepository {
+	return &LinkRepository{
+		links: make(map[string]*models.Link),
+	}
+}
+
+func (r *LinkRepository) CreateLink(link *models.Link) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	print(link.ShortCode)
+
+	if _, exists := r.links[link.ShortCode]; exists {
+		return repository.ErrLinkAlreadyExists
+	}
+
+	r.links[link.ShortCode] = link
+
+	return nil
+}
+
+func (r *LinkRepository) GetLinkByCode(code string) (*models.Link, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	link, exists := r.links[code]
+	if !exists {
+		return nil, repository.ErrLinkNotFound
+	}
+
+	return link, nil
+}
