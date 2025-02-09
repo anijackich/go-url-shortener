39376221c4ed9befeda2f7ae192f2fbbae25b287
anijackich <anijackich@gmail.com>
2025-02-09 20:32:01 +0300

    feat(app-logic): add service

--- internal/service/links.go
+++ internal/service/links.go
@@ -1,1 +1,90 @@
 package service
+
+import (
+	"go-url-shortener/internal/models"
+	"go-url-shortener/internal/repository"
+	"go-url-shortener/pkg/utils"
+	neturl "net/url"
+	"regexp"
+	"strings"
+)
+
+func isValidDomain(domain string) bool {
+	re := regexp.MustCompile(
+		`^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)(?:\.[a-z]{2,})+$`,
+	)
+
+	return re.MatchString(domain)
+}
+
+type LinkService struct {
+	domain       string
+	codeAlphabet string
+	codeLength   int
+	repo         repository.LinkRepository
+}
+
+func NewLinkService(
+	domain string,
+	codeAlphabet string,
+	codeLength int,
+	linksRepo repository.LinkRepository,
+) (*LinkService, error) {
+	if !isValidDomain(domain) {
+		return nil, ErrInvalidDomain
+	}
+
+	return &LinkService{
+		domain:       domain,
+		codeAlphabet: codeAlphabet,
+		codeLength:   codeLength,
+		repo:         linksRepo,
+	}, nil
+}
+
+func (s *LinkService) ShortenLink(url string) (string, error) {
+	shortCode := utils.GenerateRandomString(s.codeAlphabet, s.codeLength)
+
+	parsedUrl, err := neturl.ParseRequestURI(url)
+	if err != nil {
+		return "", ErrInvalidURL
+	}
+
+	err = s.repo.CreateLink(&models.Link{
+		ShortCode: shortCode,
+		URL:       parsedUrl.String(),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	shortUrl := neturl.URL{
+		Scheme: "https",
+		Host:   s.domain,
+		Path:   shortCode,
+	}
+
+	return shortUrl.String(), nil
+}
+
+func (s *LinkService) ExpandShortLink(url string) (string, error) {
+	parsedUrl, err := neturl.ParseRequestURI(url)
+	if err != nil {
+		return "", ErrInvalidURL
+	}
+
+	splitPath := strings.Split(parsedUrl.Path, "/")
+
+	if len(splitPath) != 2 {
+		return "", ErrInvalidURL
+	}
+
+	shortCode := strings.Split(parsedUrl.Path, "/")[1]
+
+	link, err := s.repo.GetLinkByCode(shortCode)
+	if err != nil {
+		return "", err
+	}
+
+	return link.URL, nil
+}
